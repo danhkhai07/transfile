c8098fd0500a2f02418d23b3143509b07f04101c
danhkhai07 <aliveomg@gmail.com>
2026-03-15 15:54:02 +0700

    change logger format

--- config/logger.go
+++ config/logger.go
@@ -13,26 +13,26 @@ const (
 type StdLogger struct {}
 
 func (ml *StdLogger) Write(s string, args ...any) (error) {
-	fmt.Fprint(os.Stdout, time.Now().Format(timeFormat) + ": ")
+	fmt.Fprint(os.Stdout, time.Now().Format(timeFormat) + " ")
 	fmt.Fprintf(os.Stdout, s, args...)
 	return nil
 }
 
 func (ml *StdLogger) Writeln(s string, args ...any) (error) {
-	fmt.Fprint(os.Stdout, time.Now().Format(timeFormat) + ": ")
+	fmt.Fprint(os.Stdout, time.Now().Format(timeFormat) + " ")
 	fmt.Fprintf(os.Stdout, s, args...)
 	fmt.Fprintf(os.Stdout, "\n")
 	return nil
 }
 
 func (ml *StdLogger) Errwrite(s string, args ...any) (error) {
-	fmt.Fprint(os.Stderr, time.Now().Format(timeFormat) + ": ")
+	fmt.Fprint(os.Stderr, time.Now().Format(timeFormat) + " ")
 	fmt.Fprintf(os.Stderr, s, args...)
 	return nil
 }
 
 func (ml *StdLogger) Errwriteln(s string, args ...any) (error) {
-	fmt.Fprint(os.Stderr, time.Now().Format(timeFormat) + ": ")
+	fmt.Fprint(os.Stderr, time.Now().Format(timeFormat) + " ")
 	fmt.Fprintf(os.Stderr, s, args...)
 	fmt.Fprintf(os.Stderr, "\n")
 	return nil

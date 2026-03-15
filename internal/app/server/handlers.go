17c2f89a5c9ecee3380da4e865564c9105fb7ae9
danhkhai07 <aliveomg@gmail.com>
2026-03-15 14:54:13 +0700

    fix with readability and handlers missing parameters

--- internal/app/server/handlers.go
+++ internal/app/server/handlers.go
@@ -1,30 +1,31 @@
 package app
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // GET /health
-func (svr *Server) getHealth(http.ResponseWriter, *http.Request) {
+func (svr *Server) getHealth(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // GET /lookup/{hash}
-func (svr *Server) lookupFile(http.ResponseWriter, *http.Request) {
+func (svr *Server) lookupFile(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // GET /download/{hash}
-func (svr *Server) downloadFile(http.ResponseWriter, *http.Request) {
+func (svr *Server) downloadFile(w http.ResponseWriter, r *http.Request) {
 
 }
 
 // POST /upload
 // {
-// 	hash: "abc123",
-// 	node_addr: "192.168.1.1:52000"
-// 	file_name: "Never_Gonna_Give_U_Up.mp4"
-// 	size: 734003200
-// 	
+// 		hash: "abc123",
+// 		node_addr: "192.168.1.1:52000"
+// 		file_name: "Never_Gonna_Give_U_Up.mp4"
+// 		size: 734003200
 // }
-func (svr *Server) postFile(http.ResponseWriter, *http.Request) {
+func (svr *Server) postFile(w http.ResponseWriter, r *http.Request) {
 
 }
